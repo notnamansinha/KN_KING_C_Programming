9f75528a384e7b5237f1f143693a77c7ad2a72a7
Naman Kumar Sinha <notnamansinha@gmail.com>
2026-02-16 23:35:25 +0530

    Add files via upload

---
+++ FormattedIO/Projects/date.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int month,day,year;
+
+    printf("Enter a date (mm/dd/yyyy) : ");
+    scanf("%d /%d /%d",&month,&day,&year);
+
+    printf("You entered the date %d%d%d",year,month,day);
+
+}
