5c90923e4eda3675d0d187d342c91592b53d6a3d
Naman Kumar Sinha <notnamansinha@gmail.com>
2026-02-19 23:56:58 +0530

    Fix formatting and add newline at end of product.c

--- FormattedIO/Projects/product.c
+++ FormattedIO/Projects/product.c
@@ -9,11 +9,11 @@ int main(void)
     printf("Enter the price: ");
     scanf("%d", &price);
 
-    printf("Enter the purchase date (mm/dd/yyyy): ");
+    printf("Enter the purchase date (mm/dd/yyyy): \n");
     scanf("%d/%d/%d",&month,&day,&year);
 
-    printf("Item\t Unit\t Purchase\n");
-    printf(" \t Price\t Date\n");
-    printf("%d \t$%d \t%d/%d/%d\n",itemno,price,month,day,year);
+    printf("Item\tUnit\tPurchase\n");
+    printf("\tPrice\tDate\n");
+    printf("%d\t$%d\t%d/%d/%d\n", itemno, price, month, day, year);
 
 }
