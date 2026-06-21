129ff4f8c5249fec1298e7d4a7140794ef3f8462
naman <notnamansinha@gmail.com>
2026-06-21 16:36:53 +0530

    add 3.c version but using horner's algorithm

---
+++ Practice/Fundamentals/3new.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int v, Loss;
+    printf("enter integer value for v: ");
+    scanf("%d", &v);
+
+    // Horner's Rule: an algorithm that optimizes
+    // polynomial evaluation by rewriting the 
+    // polynomial in a nested form
+
+    Loss = (((2*v + 3) * (v - 1)) * (v + 5)) * v;
+
+    printf("the final calculated integer is: %d", &Loss);
+    
+}
